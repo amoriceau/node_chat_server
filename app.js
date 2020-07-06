8f03c430e29a5929faa7cbc2bc65f6f1a29ca506
amoriceau <contact.amoriceau@gmail.com>
2020-07-06 23:28:45 +0200

    server without any style and functions

---
+++ app.js
@@ -0,0 +1,47 @@
+const URL = 'http://localhost'
+const PORT = 8080
+
+
+let http = require('http');
+let fs = require('fs');
+
+
+
+let server = http.createServer(function (req, res) {
+    fs.readFile('./index.html', 'utf-8', function (error, content) {
+        res.writeHead(200, {
+            "Content-Type": "text/html"
+        });
+        res.end(content);
+    });
+});
+
+let io = require('socket.io').listen(server);
+
+io.sockets.on('disconnect', function (socket) {
+    console.log('user disconnected');
+});
+io.sockets.on('connect', function (socket) {
+    socket.emit('new_con', 'Hello there');
+    console.log('new user connected');
+    socket.on('new_user', nick => {
+        socket.username = nick
+        socket.broadcast.emit("user_join", nick)
+    })
+    socket.on('client_msg', function (message) {
+        message = message.toString()
+        let usr = "anon"
+        if (socket.username !== null) {
+            usr = socket.username
+
+        }
+        socket.broadcast.emit("sent_msg", {
+            from: usr, content: message
+        })
+    });
+});
+
+
+
+server.listen(PORT);
+console.log("server running at " + URL + ":" + PORT);
